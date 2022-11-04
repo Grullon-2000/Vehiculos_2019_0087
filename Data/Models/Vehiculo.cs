142a7dbad9469d7f236efe4f31d6ffbaf7e65983
Gregori grullon <Grulln-2000>
2022-11-03 22:38:10 -0400

    0.1

--- Data/Models/Vehiculo.cs
+++ Data/Models/Vehiculo.cs
@@ -1,10 +1,8 @@
-
-using System.ComponentModel.DataAnnotations
 namespace Models;
 
 public class Vehiculo
 {
-[Key]
+
 public int vehiculoID { get; set; }
 public string Marca { get; set; } = null!;
 public string? Modelo { get; set; }                                          
